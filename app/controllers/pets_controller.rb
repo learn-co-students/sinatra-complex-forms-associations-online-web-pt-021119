ee51dd6bde29287677608aa880a04affce286fad
github username <github email address>
2019-06-01 22:21:29 -0500

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,24 +2,31 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    if params["pet"]["owner_id"]
+      @pet = Pet.create(params["pet"])
+    else
+      @owner = Owner.create(name: params["owner_name"])
+      @pet = Pet.create(name: params["pet"]["name"], owner_id: @owner.id)
+    end
 
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  patch '/pets/:id' do
 
     redirect to "pets/#{@pet.id}"
   end
