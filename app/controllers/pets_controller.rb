741efb48e0485008ec3ef2f745e5cff505f66efc
Haj B <phajib@gmail.com>
2019-06-24 17:27:35 +1000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,24 +2,48 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
+
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+    end
+    @pet.save
 
-    redirect to "pets/#{@pet.id}"
+    redirect "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
+    @owners = Owner.all
+
+    erb :'/pets/edit'
+  end
+
+  get '/pets/:id' do
+    @pet = Pet.find(params[:id])
+    @owner = @pet.owner
+
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  patch '/pets/:id' do
+    if !params[:owner][:name].empty?
+      @owner = Owner.create(name: params[:owner][:name])
+      params[:pet][:owner_id] = @owner.id
+    end
+
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
 
     redirect to "pets/#{@pet.id}"
   end
